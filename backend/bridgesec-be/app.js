412669ebce2a378833820ad626f42f37ae31d7b1
Aparana singh <aparana.singh@adjecti.com>
2024-12-20 14:06:50 +0530

    update app js and package.json file

--- backend/bridgesec-be/app.js
+++ backend/bridgesec-be/app.js
@@ -9,33 +9,46 @@ const { connectDB } = require('./db_config/mongo');
 
 const app = express(); 
 
-// CORS Configuration
+// app.use(cors({
+//   //origin: 'http://localhost:3000', // Replace with the React app’s URL
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+//   //allowedHeaders: '*',
+//   // credentials: true // Enable to allow cookies and authorization headers
+// }));
 app.use(cors({
-  origin: '*', // Your frontend's URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Timezone','application/json', "*"], // Specify allowed headers
-  credentials: true // Allow credentials (cookies, authorization headers)
+  origin: '*', // Your React app URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'], // Headers
+  credentials: true // Allow cookies and headers
 }));
 
-// Middleware to parse JSON request bodies
+app.options('*', cors());
+
+
+// Middleware
 app.use(bodyParser.json());
 
 // Routes
 app.use('/users', userRoutes);
 app.use('/dashboard', dashboardRoutes);
+app.get("/", (req,res)=>{
+  res.send("code working")
+})
 
-// Basic route for testing
-app.get("/", (req, res) => {
-  res.send("Code working");
-});
-
-// Error Handling Middleware
+// Error Handling
 app.use((err, req, res, next) => {
   logger.error(err.stack);
   res.status(500).json({ message: 'An unexpected error occurred' });
 });
 
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
 // Connect to the MongoDB database
 connectDB(); 
 
-module.exports = { app };
+module.exports = {app};
+
+
+
