c7c539f0b8cecc56edad837d991d17ebd462553f
Aparana singh <aparana.singh@adjecti.com>
2024-12-20 11:51:52 +0530

    update vercel.json and app.js

--- backend/bridgesec-be/app.js
+++ backend/bridgesec-be/app.js
@@ -16,14 +16,30 @@ const app = express();
 //   //allowedHeaders: '*',
 //   // credentials: true // Enable to allow cookies and authorization headers
 // }));
-app.use(cors({
-  origin: '*', // Your React app URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With','Timezone','application/json'], // Headers
-  credentials: true // Allow cookies and headers
-}));
 
-app.options('*', cors());
+// app.use(cors({
+//   origin: '*', // Your React app URL
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
+//   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With','Timezone','application/json'], // Headers
+//   credentials: true // Allow cookies and headers
+// }));
+
+// app.options('*', cors());
+app.use(cors({
+  origin: 'https://bridge-sec-project-frontend.vercel.app', // Your frontend's URL
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Timezone', 'application/json'],
+  credentials: true
+}));
+// Handle preflight OPTIONS request
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', 'https://bridge-sec-project-frontend.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Timezone, application/json');
+  res.sendStatus(204); // No Content
+});
+
+
 
 // // Preflight OPTIONS handling
 // app.options('*', (req, res) => {
